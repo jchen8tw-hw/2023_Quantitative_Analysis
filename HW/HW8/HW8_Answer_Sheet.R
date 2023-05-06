4277fb05e8543d8d80fa851484e0698f3357db3a
jchen8tw <11806483+jchen8tw@users.noreply.github.com>
2023-05-06 16:48:03 +0800

    add HW8

---
+++ HW/HW8/HW8_Answer_Sheet.R
@@ -0,0 +1,56 @@
+###############################################################################
+##                           HW 8 Answer Sheet         			    		         ##
+##            	        Name :______   NTU ID:________  	    	     	    	 ##
+###############################################################################
+
+##############
+# Question 1 #
+##############
+
+#####
+#(a)# 
+#####
+rm(list=ls(all=T))
+library(ISLR)
+set.seed(1)
+
+#Best model = 
+#Test MSE = 
+
+#####
+#(b)# 
+##### 
+
+
+#Best model = 
+#Test MSE = 
+
+#####
+#(c)# 
+#####
+set.seed(1)
+
+
+#Best model = 
+#Test MSE = 
+
+##############
+# Question 2 #
+##############
+
+#####
+#(a)# 
+#####
+
+#Paired Bootstrap estimation = 
+
+
+#####
+#(b)# 
+#####
+
+#Residual Bootstrap estimation = 
+
+
+
+
